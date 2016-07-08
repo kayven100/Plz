74dd40daa53e1128b80112f2ff73e6834d9c76c0
kayven100 <kayvenp100@gmail.com>
2016-07-07 20:50:52 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,5 @@
+<? include_once("ThePoor.html")?>
+<? include_once("WorldConflict.html")?>
+<? include_once("WorkingwithAnimations.html")?>
+<? include_once("HomePage.html")?>
+<? include_once("WorldConflict.html")?>
